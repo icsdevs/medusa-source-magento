a4d89f83e9c9ddf6acdacf0a6cb12f72017b0765
Dave Rosthorn <dave@icansee.co.uk>
2023-06-13 18:39:52 +0100

    Magento categories will get created as medusa categories instead of collections

--- src/strategies/import.ts
+++ src/strategies/import.ts
@@ -75,7 +75,7 @@ class ImportStrategy extends AbstractBatchJobStrategy {
 
     // retrieve categories
     this.logger_.info('Importing categories from Magento...');
-    const { data } = await this.magentoClientService_.retrieveCategories();//lastUpdatedTime);
+    const { data } = await this.magentoClientService_.retrieveCategories(lastUpdatedTime);
     data.items.map(async (category) => {
       return this.magentoCategoryService_.create(category);
     })
